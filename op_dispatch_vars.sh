75ae37e66a33821dae69e498925315ff8d867f1e
SpencerTSmith <spencertsmith2003@gmail.com>
2024-10-10 09:38:18 -0500

    modify make to reflect changes in repo structure

--- op_dispatch_vars.sh
+++ op_dispatch_vars.sh
@@ -15,9 +15,9 @@ OP_BASELINE_FILE="baseline_op.c"    #
 # HOWEVER, CHANGE THESE LINES:             #
 # Replace the filenames with your variants #
 ############################################
-OP_SUBMISSION_VAR01_FILE="tuned_variant01_op.c"
-OP_SUBMISSION_VAR02_FILE="tuned_variant02_op.c"
-OP_SUBMISSION_VAR03_FILE="tuned_variant03_op.c"
+OP_SUBMISSION_VAR01_FILE="./src/tuned_variant01_op.c"
+OP_SUBMISSION_VAR02_FILE="./src/tuned_variant02_op.c"
+OP_SUBMISSION_VAR03_FILE="./src/tuned_variant03_op.c"
 
 ######################################################
 # You can even change the compiler flags if you want #
